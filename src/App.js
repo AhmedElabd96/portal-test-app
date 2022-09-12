20f5478c475b74444afb1380a6c27062e7bddb8d
AhmedMounir <a.mounir@penta-b.com>
2022-09-12 17:38:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const handleClick = (e) => {
+    e.preventDefault();
+    const frame = document.getElementById('iframe');
+    const malib = frame.contentWindow.MaLib;
+    let queryParameters = [
+      {
+        "dataSource": {
+          "id": 73510
+        },
+        "filter": {
+          "logicalOperation": "OR",
+          "conditionList": [
+            {
+              "tabularCondition": {
+                "key": "code",
+                "dataType": "STRING",
+                "operator": "=",
+                "value": "01470402014000000000000000"
+              }
+            }
+          ]
+        },
+        "returns": []
+      }
+    ]
+
+    malib.query.queryFeatures(queryParameters).then(res => {
+      let features = JSON.parse(res.data[0].features)
+      let geometry = res && res.data && features.features[0].geometry
+      malib.apiRegistry
+        .getApis(["Feature"])
+        .then(([Feature]) => {
+          let fe = new Feature({
+            type: "Feature",
+            geometry: geometry,
+            properties: {},
+          });
+
+          malib.actionsRegistry.dispatch("setMapBBOX", fe.getBBox());
+          malib.actionsRegistry.dispatch("addHighlight", fe);
+        });
+    })
+  }
+  return (
+      <div className="App">
+        <div>
+          Test Form
+          <button onClick={handleClick}>CLICK</button>
+        </div>
+        <iframe
+          id="iframe"
+          className='iframe'
+          src="http://newcapital.eastus.cloudapp.azure.com:8080/portal"
+        ></iframe>
+      </div>
+    );
+  }
+
+  export default App;
